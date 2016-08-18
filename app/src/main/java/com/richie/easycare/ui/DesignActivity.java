59f1c674f2965a178d87b4957ae3357fb6badfd9
liqi <liqi@wondersgroup.com>
2016-08-18 23:08:44 +0800

    add sample code

--- app/src/main/java/com/richie/easycare/ui/DesignActivity.java
+++ app/src/main/java/com/richie/easycare/ui/DesignActivity.java
@@ -2,16 +2,29 @@ package com.richie.easycare.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import com.richie.easycare.R;
 
 public class DesignActivity extends Activity {
+
+    private GridView gridView;
+    private String[] titles = {"感冒","发烧","咳嗽","头痛","上火","更多..."};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_design);
 
+        init();
 
+    }
+
+    private void init(){
+        gridView = (GridView) findViewById(R.id.gridview_main);
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.item_gridview_main,R.id.item_text,titles);
+        gridView.setAdapter(adapter);
     }
 }
